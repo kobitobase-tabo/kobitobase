b1b78145f8e7180c06aa6b53b49d9f717e7869f2
kobitobase-tabo <kobitononiwa@gmail.com>
2025-11-08 09:24:00 +0900

    fix sanity client configuration

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -1,15 +1,9 @@
 import { createClient } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
+import { apiVersion, dataset, projectId } from "./env";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "2023-01-01",
-  useCdn: true,
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true, // 高速読み込み (最新が必要なら false)
 });
-
-const builder = imageUrlBuilder(client);
-
-export function urlFor(source) {
-  return builder.image(source);
-}
